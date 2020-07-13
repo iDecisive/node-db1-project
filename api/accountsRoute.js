cbb0200071818501a1cc5f02cc365903c1ff36e9
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-13 00:14:22 -0400

    Post works

--- api/accountsRoute.js
+++ api/accountsRoute.js
@@ -5,15 +5,11 @@ const db = require('../data/dbConfig.js');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-	console.log(req.body);
 
 	db('accounts')
-		.insert({
-			name: req.name,
-			budget: req.budget,
-		})
-		.then(() => {
-			res.status(201).json({ message: 'worked!' });
+		.insert(req.body, 'id')
+		.then((id) => {
+			res.status(201).json(id);
 		})
 		.catch((err) => {
 			res.status(500).json({ error: 'Server error', details: err });
